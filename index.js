5f9897421901590c2e4d1d58ae7ea0525d549ce5
sujal patel <worldonwheels701@gmail.com>
2024-02-02 19:49:37 -0500

    basic routing and storing data in array

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 import express from 'express';
-
+import path from 'path';
 const app = express();
 
+
+const users=[];
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(path.resolve(), 'public')));
+//use middle ware
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index', { name: 'John', age: 30, address: 'New York'});
+    res.render('index');
 });
+app.get('/success', (req, res) => {
+    res.render("success");
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.phone);
+     
+    users.push({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+    
+    });
+    res.render('success', { users: users });
+    res.redirect('/success');
+    res.send('Data received');
+
+});
+
 
 app.get('/about', (req, res) => {
     res.send('About Page');
